c613776bd71561132bbff62f911106b361efd5e0
Calum <Calum@Calum-PC>
2013-10-14 19:37:02 +0100

    Implemented HashDictionary(isPrime, increase tablesize, isoverloaded)

--- DSACoursework/src/com/calumturner/HashDictionary.java
+++ DSACoursework/src/com/calumturner/HashDictionary.java
@@ -3,18 +3,21 @@ package com.calumturner;
 import java.util.Iterator;
 
 public class HashDictionary implements Dictionary {
-
+	private String[] table = new String[6];
+	private int numElements;
 	private float loadF;
-	StringHashCode hashCode;
+	private StringHashCode hashCode;
 	public HashDictionary(StringHashCode sH, float lF) {
 		loadF= lF;
-		hashCode= hashCode;
+		hashCode= sH;
+		numElements=0;
 	}
 	
 	public HashDictionary()throws DictionaryException{}
 	
 	@Override
 	public void insert(String key) throws DictionaryException {
+		if(isOverLoaded(numElements+1))increaseTable();
 		int hCode = hashCode.giveCode(key);
 		
 		
@@ -39,7 +42,48 @@ public class HashDictionary implements Dictionary {
 		return null;
 	}
 	
-	private 
+	private void increaseTable(){
+		int tableSize = table.length*2; //Start looking for the next prime number double the size of the old size.
+		if(tableSize%2==0)tableSize++;//If even make odd.
+		while(!isPrime(tableSize))//Recur through odd numbers till a prime number is located.
+			tableSize+=2;
+		String newTable[] = table;
+		table= new String[tableSize]; //Set table to new prime number size
+	
+		for(int i=0;i<table.length;i++){ //Reinsert all the odd values into the new sized hash table
+			try {
+				insert(newTable[i]);
+			} catch (DictionaryException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	
+	private boolean isPrime(int num){
+		//If num is even, not prime.
+		if(num%2==0)return false;
+		//Check odd numbers returning if not prime.
+		for(int i = 3; i*i<=num; i+=2)
+			if(num%i==0)return false;
+		
+		return true;
+	}
+	
+	private boolean isOverLoaded(int numE){
+		if((float)numE/table.length>loadF)return true;
+		return false;
+		
+	}
+	
+	private int compressKey(){
+		
+		
+		
+		return 0;		
+	}
 	
 	
 
